bd348a4d1c595b89c63b0a941299bfdb22caab10
unknown <mopri38@gmail.com>
2021-10-29 16:40:47 -0500

    craete folders in views

--- src/app.js
+++ src/app.js
@@ -7,27 +7,27 @@ app.use(express.static(path.resolve(__dirname, './public')));
 
 //home
 app.get('/', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, './views/index.html'));
+    res.sendFile(path.resolve(__dirname, './views/products/index.html'));
 });
 
 //login
 app.get('/login', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
+    res.sendFile(path.resolve(__dirname, './views/users/login.html'));
 });
 
 //register
 app.get('/register', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
+    res.sendFile(path.resolve(__dirname, './views/users/register.html'));
 });
 
 //productPage
 app.get('/product', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, './views/product.html'));
+    res.sendFile(path.resolve(__dirname, './views/products/product.html'));
 });
 
 //cart
 app.get('/cart', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, './views/cart.html'));
+    res.sendFile(path.resolve(__dirname, './views/products/cart.html'));
 });
 
 app.listen(port, ()=>{
