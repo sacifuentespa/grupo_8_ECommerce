f1a76f21d426d9c0503afb066f1e3c9d6c4ccdb1
Santiago Cifuentes <sacifuentespawk@gmail.com>
2022-02-11 18:23:38 -0500

    mainroutes

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -15,52 +15,6 @@ const comprobationGuest = require("../middleware/comprobationGuest")
 
 //home
 router.get("/", mainController.getIndex);
-router.post("/search", productsController.search)
-
-//productPage
-router.get("/product/:id", productsController.getProduct);
-
-
-//productPageUpload
-router.get("/productUpload", productsController.getNewProduct);
-router.post("/productUpload", cpUploadProduct, validations, productsController.uploadNewProduct);
-
-//productEdition
-router.get("/productEdit/:id", productsController.getUpdateProduct);
-router.put("/productEdit", cpUploadProduct, validationsEdit, productsController.uploadUpdateProduct);
-
-
-//cart
-router.get("/cart", mainController.getCart);
-
-//login
-router.get("/login", comprobationLogged, usersController.getLogin);
-router.post("/login", usersController.comprobationLogin)
-
-//register
-router.get("/register", comprobationLogged, usersController.getRegister);
-router.post("/register", cpUploadUser, validationRegister, usersController.uploadNewUser);
-
-//profile
-router.get("/users/userProfile", comprobationGuest,  usersController.getProfile)
-
-//userEdition
-router.get("/users/userEdit", comprobationGuest, usersController.getUpdateUser);
-router.put("/users/userEdit", cpUploadUser, validationRegister, usersController.updateUser);
-
-
-//list all products
-router.get("/listProducts", productsController.getProducts)
-
-//logout
-router.get("/logout", usersController.logOut)
-router.post("/logout", usersController.logOut)
-
-//admin
-router.get("/admin/products/:password", adminController.getAllProducts)
-router.get("/admin/users/:password", adminController.getAllUsers)
-router.delete("/deleteProduct/:id", productsController.deleteProduct)
-router.delete("/deleteUser/:id", usersController.deleteUser)  
 
 
 module.exports = router;
