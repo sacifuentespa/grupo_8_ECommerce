915da5f4295356e5a0a66ace792a24e24503b5e8
Santiago Cifuentes <sacifuentespawk@gmail.com>
2022-02-11 18:24:09 -0500

    mainroutes

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -1,16 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const mainController = require("../controllers/mainController");
-const productsController = require("../controllers/productsController");
-const usersController = require("../controllers/userController");
-const adminController = require("../controllers/adminController")
-const cpUploadProduct = require("../middleware/uploadFileProduct")
-const validations = require("../middleware/validationUpload")
-const validationsEdit = require("../middleware/validationEdit")
-const cpUploadUser = require("../middleware/uploadFileUser");
-const validationRegister = require("../middleware/validationRegister")
-const comprobationLogged = require("../middleware/comprobationLogged")
-const comprobationGuest = require("../middleware/comprobationGuest")
 
 
 //home
