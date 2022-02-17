a4254415b92343d46e177029beb303085f6c168e
Santiago Cifuentes <sacifuentespawk@gmail.com>
2022-02-16 20:50:17 -0500

    front validations for products fix

--- src/public/js/front-validations/productsValidator.js
+++ src/public/js/front-validations/productsValidator.js
@@ -3,7 +3,7 @@ let form = document.querySelector(".formProductUpload");
 
 form.onsubmit = async (e) => {
     e.preventDefault();
-    console.log('wola')
+    
 
     let productName = document.querySelector("input[name=productName]").value;
     if(!productName  || productName.length < 5){
@@ -11,11 +11,34 @@ form.onsubmit = async (e) => {
         return false;
     }
 
-    let productDescriptionUpload = document.querySelector("input[name=productDescriptionUpload]").value;
+    let productPrice = document.querySelector("input[name=productPrice]").value;
+    if(!productPrice){
+        showError("El producto debe tener un precio estimado");
+        return false;
+    }
+
+    let productDescriptionUpload = document.querySelector("textarea[name=productDescriptionUpload]").value;
     if(!productDescriptionUpload || productDescriptionUpload.length < 20){
         showError("Una descripción debe tener al menos 20 caracteres");
         return false;
     }
+
+    let mainImageUpload = document.querySelector("input[name=mainImageUpload]").value
+    console.log(mainImageUpload)
+    if(!mainImageUpload){
+        showError("El producto debe tener una imagen principal");
+        return false;
+    }
+
+    let aimUpload = document.querySelector("input[name=aimUpload").value;
+    
+    if(!aimUpload){
+        showError("Debemos saber qué quieres hacer con tu producto");
+        return false;
+    }
+    
+
+    
     form.submit();
 }
 
